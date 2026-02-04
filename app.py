c3a8d76962572c8667615446df505d251208568b
nyczraf <nyczraf@gmail.com>
2026-02-04 12:03:24 +0100

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from datetime import date
 st.set_page_config(page_title="Logistyka Paliwowa", layout="centered", page_icon="⛽")
 
 # --- KONFIGURACJA ---
-URL = "https://docs.google.com/spreadsheets/d/1pEuOX5WoOhv-JMwzvmHPcMbMttEaK5Vsc8-mgja569o/edit?gid=845295439#gid=845295439"
+URL = https://docs.google.com/spreadsheets/d/1pEuOX5WoOhv-JMwzvmHPcMbMttEaK5Vsc8-mgja569o/edit?gid=845295439#gid=845295439
 conn = st.connection("gsheets", type=GSheetsConnection)
 
 def load_data():
@@ -82,4 +82,5 @@ with st.expander("🔐 Administracja (Kasowanie)"):
             st.rerun()
         else:
             st.error("Błędne hasło!")
+
 
